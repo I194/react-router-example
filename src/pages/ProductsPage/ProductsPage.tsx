cae79716d3f479822be592637331bcddf84671cf
I194 <i1948374@yandex.ru>
2022-11-18 09:20:40 +0300

    fixed grammar

--- src/pages/ProductsPage/ProductsPage.tsx
+++ src/pages/ProductsPage/ProductsPage.tsx
@@ -6,9 +6,9 @@ const ProductsPage = () => {
   return (
     <div className={styles.container}>
       <h2>Products Page</h2>
-      <Product name='Green scarf' phrase="For a showy winter" price={5} />
-      <Product name='Red gloves' phrase="For a showy winter" price={5} />
-      <Product name='Red hat' phrase="For a showy winter" price={5} />
+      <Product name='Green scarf' phrase="For a snowy winter" price={5} />
+      <Product name='Red gloves' phrase="For a snowy winter" price={5} />
+      <Product name='Red hat' phrase="For a snowy winter" price={5} />
     </div>
   )
 };
